40f0f16b5c12488c7733fed1fe106d8a8c0864b2
ANEESH AASHIFA <aneeshaashifa6@gmail.com>
2025-09-14 16:13:20 +0530

    updated ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive,MatButtonModule,MatIconModule,MatToolbarModule],
+  imports: [RouterOutlet,CommonModule, RouterLink, RouterLinkActive,MatButtonModule,MatIconModule,MatToolbarModule],
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+   get isLoggedIn(): boolean {
+    // 👈 reactive check every time Angular renders
+    return !!localStorage.getItem('token');
+  }
+
+  constructor(private router: Router) {}
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/']); // go back to home
+  }
+}
