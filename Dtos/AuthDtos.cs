d81611ec9113de44adb64e9922737fd27fa57b12
ANEESH AASHIFA <aneeshaashifa6@gmail.com>
2025-09-18 20:49:27 +0530

    added frontend and backend

--- Dtos/AuthDtos.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TruYum.Api.Dtos
-{
-    public record RegisterDto(string Username, string Password, string Role);
-    public record LoginDto(string Username, string Password);
-    public record TokenResponse(string Token);
-}
